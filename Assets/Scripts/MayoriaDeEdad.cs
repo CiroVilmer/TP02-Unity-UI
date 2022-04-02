dc7d536a91068836dfc26fc65a9215ab355ebe75
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-04-01 21:00:53 -0300

    tutorial n°3 done

--- Assets/Scripts/MayoriaDeEdad.cs
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Tutorial1 : MonoBehaviour
+public class MayoriaDeEdad : MonoBehaviour
 {
-
     public int edadUsuario;
+    public Text miTexto;
 
     void Start()
     {
-
-       if (edadUsuario >= 18)
+        if (edadUsuario >= 18)
         {
+            miTexto.text = "Es mayor de edad";
             Debug.Log("Es mayor de edad");
         }
         else
         {
+            miTexto.text = "No es mayor de edad";
             Debug.Log("No es mayor de edad");
         }
     }
-
 }
