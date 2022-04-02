f54db9fc148261c4023ae89540d84ca3a255e1f3
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-04-01 22:46:49 -0300

    tutorial n°4, n°5 and n°6 done

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public GameObject cartelitoMsj;
+    public Text textoMsj;
+
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            textoMsj.text = "Bienvenido";
+            cartelitoMsj.SetActive(true);
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            textoMsj.text = "Contraseña Incorrecta";
+            cartelitoMsj.SetActive(true);
+            Debug.Log("Contrasenia Incorrecta");
+        }
+    }
+}
